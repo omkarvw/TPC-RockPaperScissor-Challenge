4bc63223876727fb4e4dd77f59f436a00dc26332
omkarvw <womkar0203@gmail.com>
2022-12-24 16:07:48 +0530

    Changed position of RuleBanner

--- src/Components/TitleBoard.js
+++ src/Components/TitleBoard.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Score from './Score'
-import RuleContainer from './RuleContainer'
 
 function TitleBoard(props) {
   return (
     <div id='title-board'>
         <img src={require('./Images/logo.svg').default} alt="" id='logoImg' />
-        {/* <RuleContainer></RuleContainer> */}
-        {/* <div>Rules</div> */}
         <Score scoreCounter= {props.scoreCounter} ></Score>
     </div>
   )
